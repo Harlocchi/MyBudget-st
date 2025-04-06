7326a0eb5658206db59c7bc06042340230b7feca
Harlocchi <gustaalves1701@gmail.com>
2025-04-06 03:10:12 -0300

    add adsense

--- main.py
+++ main.py
@@ -22,17 +22,16 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+adsense_code = """
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9500154758800356"
+     crossorigin="anonymous">
+     </script>
+"""
 
-# st.components.v1.html("""
-#     <div style="margin: 20px 0; text-align: center;">
-#         <ins class="adsbygoogle"
-#              style="display:block"
-#              data-ad-client="ca-pub-9500154758800356"
-#              data-ad-slot="1234567890"
-#              data-ad-format="auto"></ins>
-#         <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-#     </div>
-# """, height=200)
+# Insere o código HTML no app
+components.html(adsense_code, height=150)
+
+
 
 
 st.title("Orçamento - Itens do Serviço")
